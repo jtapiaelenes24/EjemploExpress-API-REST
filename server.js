d0d35bd53ce612d3be032ef028515d8822912e25
JT <jtapiaelenes24@gmail.com>
2024-09-12 20:26:34 +0200

    Añadido el funcionamiento de los métodos get y post

--- server.js
+++ server.js
@@ -1,32 +1,48 @@
-const express = require('express')
-const morgan = require('morgan')
+const express = require("express");
+const morgan = require("morgan");
 
-const app = express()
+const app = express();
+const products = [
+  {
+    id: 1,
+    name: "laptop",
+    price: 3000,
+  },
+];
 
-app.use(morgan('dev'))
+app.use(morgan("dev"));
+app.use(express.json());
 
-app.get('/products', (req, res) => {
-    console.log('Obteniendo productos')
-})
+app.get("/products", (req, res) => {
+  res.json(products);
+});
 
-app.post('/products', (req, res) => {
-    console.log('Creando productos')
-})
+app.post("/products", (req, res) => {
+  const newProduct = { ...req.body, id: products.length + 1 };
+  products.push(newProduct);
+  res.send(newProduct);
+});
 
+app.put("/products", (req, res) => {
+  res.send("Actualizando productos");
+});
 
-app.put('/products', (req, res) => {
-    console.log('Actualizando productos')
-})
+app.delete("/products", (req, res) => {
+  res.send("Eliminando productos");
+});
 
+app.get("/products/:id", (req, res) => {
+  console.log(req.params.id);
+  const productFound = products.find((p) => p.id === parseInt(req.params.id));
 
-app.delete('/products', (req, res) => {
-    console.log('Eliminando productos')
-})
+  if (!productFound)
+    return res.status(404).json({
+      message: "Product not found",
+    });
 
+  console.log(productFound);
+  res.json(productFound);
+});
 
-app.get('/products/:id', (req, res) => {
-    console.log('Obteniendo un producto')
-})
-
-app.listen(3000)
-console.log(`Server on port ${3000}`)
+app.listen(3000);
+console.log(`Server on port ${3000}`);
